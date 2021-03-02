ad7c76016291d096ddb8750fb7ef748c5de11c76
matteoaricci <matteo.a.ricci@gmail.com>
2021-03-02 14:06:23 -0500

    react router and basic setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAHruCmAngYPmafJLbmkcOSfUqu3sAEiuA",
+  authDomain: "verbose-2e4ab.firebaseapp.com",
+  projectId: "verbose-2e4ab",
+  storageBucket: "verbose-2e4ab.appspot.com",
+  messagingSenderId: "938737527012",
+  appId: "1:938737527012:web:e6afe8be3054421ca7a4f1",
+};
