9a209e26de4a605347192eb293357d74eec713b6
matteoaricci <matteo.a.ricci@gmail.com>
2021-03-08 10:35:07 -0500

    can send message

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { Button } from "@material-ui/core";
+import { db } from "../firebase";
+import firebase from "firebase";
 
-const ChatInput = () => {
-  return <ChatInputContainer></ChatInputContainer>;
+const ChatInput = ({ channelName, channelId }) => {
+  const [message, setMessage] = useState("");
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    if (!channelId) {
+      return false;
+    }
+
+    db.collection("rooms").doc(channelId).collection("messages").add({
+      message: message,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      user: "Matteo",
+      
+    });
+  };
+  return (
+    <ChatInputContainer>
+      <form onSubmit={e => sendMessage(e)}>
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder={"Message " + "ROOM"}
+          variant="outlined"
+        />
+        <Button hidden type="submit">
+          SEND
+        </Button>
+      </form>
+    </ChatInputContainer>
+  );
 };
 
 export default ChatInput;
 
-const ChatInputContainer = styled.div``;
+const ChatInputContainer = styled.div`
+  border-radius: 20px;
+
+  > form {
+    position: relative;
+    display: flex;
+    justify-content: center;
+  }
+
+  > form > input {
+    position: fixed;
+    bottom: 30px;
+    width: 60%;
+    border: 1px solid gray;
+    border-radius: 3px;
+    padding: 20px;
+    outline: none;
+  }
+
+  > form > button {
+    display: none !important;
+  }
+`;
